5e8dc122a8f8092409f32dee3923418d3109178b
Falua Temitope Oyewole <emmat0616@gmail.com>
2017-12-01 14:00:45 +0100

    test

--- test2.php
+++ test2.php
@@ -7,7 +7,34 @@ $referenceTable['val3'] = array(4, 5);
 
 $testArray = array();
 
-$testArray = array_merge($testArray, $referenceTable['val1']);
+$testArray = array_merge($testArray, (array)$referenceTable['val1']);
 var_dump($testArray);
-$testArray = array_merge($testAray, (array)$referenceTable['val2']);
+$testArray = array_merge($testArray, (array)$referenceTable['val2']);
 var_dump($testArray);
+
+$name = 'John';
+$name[10] = 'Doe';
+
+echo $name;
+
+var_dump(PHP_INT_MAX + 1);
+
+echo "Sorting";
+
+array(
+    'a' => 'z1',
+    'b' => 'za'
+);
+
+
+var_dump(0123 == 123);
+
+var_dump(0123 === 123);
+
+var_dump('0123' == 123);
+
+    $x = NULL;
+if('0xFF' == 255 ) {
+    $x = (int)'0xFF';
+    echo $x;
+}
